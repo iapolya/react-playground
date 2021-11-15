79a3df7d186251940eb6445fe5e27398167800c1
iapolya <polina7alikina@gmail.com>
2021-11-15 21:19:00 +0300

    + использование хуков useState, useEffect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,43 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      cats: [],
-      searchField: ''
-    }
-  }
+const App = () => {
+  const [cats, setCats] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const [count, setCount] = useState(0);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => {
-        this.setState({cats: users})
+        setCats(users);
       })
+  }, [])
+
+  function onSearchChange(event) {
+    setSearchField(event.target.value);
   }
 
-  onSearchChange = (event) => {
-    this.setState({searchField: event.target.value});
-  }
+  let filteredCats = cats.filter(cat => {
+    return cat.name.toLowerCase().includes(searchField.toLowerCase())
+  })
 
-  render() {
-    const {cats, searchField} = this.state;
-    const filteredCats = cats.filter(cat => {
-      return cat.name.toLowerCase().includes(searchField.toLowerCase())
-    })
-    return !cats.length ? <h1>Loading</h1> : (
-      <div className="tc">
-        <h1>MURRR</h1>
-        <Scroll>
-          <ErrorBoundry>
-            <SearchBox searchField={searchField} searchChange={this.onSearchChange}/>
-            <CardList cats={filteredCats}/>
-          </ErrorBoundry>
-        </Scroll>
-      </div>
-    )
-  }
+  return !cats.length ? <h1>Loading</h1> : (
+    <div className="tc">
+      <h1>MURRR</h1>
+      <button onClick={setCount}>Click me!</button>
+      <Scroll>
+        <ErrorBoundry>
+          <SearchBox searchField={searchField} searchChange={onSearchChange}/>
+          <CardList cats={filteredCats}/>
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  )
 }
 
 export default App;
