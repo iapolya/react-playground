b3c2cef86c07445fd66b4e3e6086ecef63ae1137
iapolya <polina7alikina@gmail.com>
2022-01-02 14:38:22 +0500

    + eslint

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
-const Card = () => {
-  const { id, name, email } = this.props;
+const Card = (props) => {
+  const { id, name, email } = props;
   return (
     <div className="bg-light-pink dib br3 pa3 ma2 grow pointer shadow-2">
       <img src={`https://robohash.org/${id}?set=set4&size=150x150`} alt="cat" />
